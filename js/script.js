da67c73ba93c79c16e9163e0ce1559542b1509a5
Vinicius Terra <viniterra33@gmail.com>
2022-04-20 13:26:58 -0300

    Tela Criação do Quizz (Info Basicas)

---
+++ js/script.js
@@ -0,0 +1,86 @@
+
+let tituloQuizz = "";
+let urlQuizz = "";
+let quantidadePerguntas = 0;
+let quantidadeNiveis = 0;
+let res
+
+criarQuizz ();
+
+function criarQuizz () {
+    infoBasicas();
+}
+
+function infoBasicas () {
+
+    let Main = document.querySelector("main");
+
+    Main.innerHTML = `
+    <h3>Comece pelo começo</h3>
+    <div class="PerguntasBasicas">
+        <input class="Title" type="text" placeholder="Título do seu quizz">
+        <input class="URL" type="text" placeholder="URL da imagem do seu quizz">
+        <input class="QuantPerg" type="text" placeholder="Quantidade de perguntas do quizz">
+        <input class="QuantNiv" type="text" placeholder="Quantidade de níveis do quizz">
+    </div>
+    <button onclick="processarInfoBasicas()">Prosseguir pra criar perguntas</button>`
+
+   
+}
+
+function processarInfoBasicas() {
+
+    tituloQuizz = document.querySelector("input.Title").value;
+    urlQuizz = document.querySelector("input.URL").value;
+    quantidadePerguntas = document.querySelector("input.QuantPerg").value;
+    quantidadeNiveis = document.querySelector("input.QuantNiv").value;
+
+    console.log(tituloQuizz.length)
+
+    if (infoBasicasValidas()) {
+
+        console.log("deu")
+        criarPerguntasQuizz();
+
+    }
+    else {
+        alert ("Informações invalidas, tente novamente");
+        infoBasicas();
+    }
+}
+
+function infoBasicasValidas() {
+    
+    let titulo = false
+    let url = false
+    let Perg  = false
+    let Nivs = false
+ 
+    if (tituloQuizz.length >= 20 && tituloQuizz.length <= 65) {
+        titulo = true
+    }
+    if (isImgLink(urlQuizz)) {
+        url = true
+    }
+    if (quantidadePerguntas >= 3) {
+        Perg = true
+    }
+    if (quantidadeNiveis >= 2) {
+       Nivs = true
+    }
+
+    if (titulo === true && url === true && Perg === true && Nivs === true ){
+        return true
+    }
+
+    return false
+}
+
+let isImgLink = (urlQuizz) => {
+
+    console.log("arrow")
+    if (typeof urlQuizz !== 'string') {
+        return false;
+    }
+    return (urlQuizz.match(/^http[^\?]*.(jpg|jpeg|gif|png|tiff|bmp)(\?(.*))?$/gmi) !== null);
+  }
